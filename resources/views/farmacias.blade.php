1757113695ccf356c46f6969629c725a0154c314
Daniel Abraham Dzul Pech <danieldayamond@gmail.com>
2021-04-14 11:29:30 -0500

    agregacion de vistas

---
+++ resources/views/farmacias.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.master')
+@section ('contenido')
+@section('titulo')
+<title>Farmacias</title>
+@endsection
+<div>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+              <img src="img/img-14.jpg" class="d-block w-100" alt="..." height="701px">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img/img15.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/img15.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+    </div>
+</div>
+
+<!--Productos-->
+<div class="container">
+  <h1 class="hola">Medicamentos Disponibles</h1>
+  <div class="row justify-content-center">
+      <div class="col-md-4 py-3">
+          <div class="card shadow" style="width: 18rem;">
+              <div class="inner">
+                  <img src="img/img-14.jpg" class="card-img-top" alt="...">
+              </div>
+              <div class="card-body text-center">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-success">Go somewhere</a>
+              </div>
+          </div>
+      </div>
+  
+
+      <div class="col-md-4 py-3">
+          <div class="card shadow" style="width: 18rem;">
+              <div class="inner">
+                  <img src="img/img-14.jpg" class="card-img-top" alt="...">
+              </div>
+              <div class="card-body text-center">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-success">Go somewhere</a>
+              </div>
+          </div>
+      </div>
+
+      <div class="col-md-4 py-3">
+          <div class="card shadow" style="width: 18rem;">
+              <div class="inner">
+                  <img src="img/img-14.jpg" class="card-img-top" alt="...">
+              </div>
+              <div class="card-body text-center">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-success">Go somewhere</a>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+@endsection
