06cfee0d7df88cd3f52a69bfacfa215f2485937f
Saul3645 <svazquez1962@gmail.com>
2021-04-15 20:24:34 -0500

    Boton lista admin

--- resources/views/admindash.blade.php
+++ resources/views/admindash.blade.php
@@ -11,29 +11,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <a class="btn btn-danger" href=" {{route('Sucursales.index')}}">Ver Lista De Farmacias</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="formulario__sucursal">
-                        <table class="table table-hover">
-                            <thead>
-                              <tr>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Descripción</th>
-                                <th scope="col">Telefono</th>
-                                <th scope="col">Direccion</th>
-                                <th scope="col">Imagen</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                          </table>
-                    </div>
+                    <a class="btn btn-danger"  href=" {{route('Sucursales.index')}}">Ver Lista De Farmacias</a>
                 </div>
             </div>
         </div>
