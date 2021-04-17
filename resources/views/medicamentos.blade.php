e63207544f632990c3b63f32e2bbd595ef674924
Saul3645 <svazquez1962@gmail.com>
2021-04-17 11:54:57 -0500

    Nada

--- resources/views/medicamentos.blade.php
+++ resources/views/medicamentos.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.master')
 @section('titulo')
-Medicamentos
+    Medicamentos
 @endsection
 <link rel="stylesheet" type="text/css" href="css/medicamentos.css">
-@section ('contenido')
+@section('contenido')
     <div>
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
-              <div class="carousel-item active">
-                  <img src="img/farmacia.jpg" class="d-block w-100" alt="..." height="701px">
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                  </div>
-              </div>
+                <div class="carousel-item active">
+                    <img src="img/farmacia.jpg" class="d-block w-100" alt="..." height="701px">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -28,21 +28,8 @@ Medicamentos
                     </div>
                     <div class="card-body text-center">
                         <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-success">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        
-
-            <div class="col-md-4 py-3">
-                <div class="card shadow" style="width: 18rem;">
-                    <div class="inner">
-                        <img src="img/img-14.jpg" class="card-img-top" alt="...">
-                    </div>
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
                         <a href="#" class="btn btn-success">Go somewhere</a>
                     </div>
                 </div>
@@ -55,7 +42,22 @@ Medicamentos
                     </div>
                     <div class="card-body text-center">
                         <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-success">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 py-3">
+                <div class="card shadow" style="width: 18rem;">
+                    <div class="inner">
+                        <img src="img/img-14.jpg" class="card-img-top" alt="...">
+                    </div>
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
                         <a href="#" class="btn btn-success">Go somewhere</a>
                     </div>
                 </div>
